233e5f677f1b15775d37c32df957565ef512b283
BenMarmont <bm0782242@gmail.com>
2023-01-27 13:59:22 +1300

    Teaching demonstration

---
+++ Teaching demonstration 27 1 23.R
@@ -0,0 +1,14 @@
+# Feedback:
+# Postive
+#   * Pace was good, he disagreed that I should have gone faster
+#     * If I am unsure about pace, ask learners whom can offer feedback
+#   * Showing file location in explorer was good
+#   * The help and about read_csv was good
+#   * Repeating how to do things in earlier lessons is not a bad thing
+
+# Improve
+#   * Give more time to context i.e. context, what we expect out of the lesson and what 
+#     * will be happening 
+#   * Didn't explain the pre-written code, definitely need to explain everything
+#   * I showed how to execute with highlight + run, but didn't show the keyboard shortcut 
+#   * If you type something, keep it. If you delete, explain why
